dd4592ef6f4bdeaafd8af1c98ae7de285ee909bb
Abhishek <abhishek84411@gmail.com>
2025-08-12 23:26:44 +0530

    Add constructor benefits and Fraction class implementation with arithmetic operations

--- DSMP 1.0/9. Session 7 - OOP Part 1 | Class & Object /OOP Part 1 | Class & Object.md
+++ DSMP 1.0/9. Session 7 - OOP Part 1 | Class & Object /OOP Part 1 | Class & Object.md
@@ -19,3 +19,11 @@ Object is an instance of the class.
 
 ## Methods vs Functions
 If there is any function inside a class then it's called Method. But if there is a function outside the class then it's called Function.
+
+## What are the benifits of constructor
+
+1. `Automatic Initialization`: When you create an object, the constructor runs automatically without you having to call it manually.
+
+2. `Code Reusability`: Instead of writing initialization code every time you create an object, you define it once in the constructor.
+
+3. `Encapsulation of Setup Logic`: You can put all the "setup" or "preparation" logic inside the constructor so objects are always ready to use.
--- DSMP 1.0/9. Session 7 - OOP Part 1 | Class & Object /script.py
+++ DSMP 1.0/9. Session 7 - OOP Part 1 | Class & Object /script.py
@@ -87,3 +87,51 @@ class Atm:
 atmObj = Atm()
 print(type(atmObj)) # <class '__main__.atm'>
 print(atmObj.balance) # 0
+
+
+
+## Create custom datatype: fraction
+class Fraction:
+    # parameterized constructor
+    def __init__(self, x, y):
+        self.num = x
+        self.den = y
+    
+    def __str__(self):
+        return "{}/{}".format(self.num, self.den)
+    
+    def __add__(self, other):
+        new_num = ((self.num * other.den) + (other.num * self.den))
+        new_den = self.den * other.den
+        return "{}/{}".format(new_num, new_den)
+    
+    def __sub__(self, other):
+        new_num = ((self.num * other.den) - (other.num * self.den))
+        new_den = self.den * other.den
+        return "{}/{}".format(new_num, new_den)
+
+    def __mul__(self, other):
+        new_num = self.num * other.num
+        new_den = self.den * other.den
+        return "{}/{}".format(new_num, new_den)
+
+    def __truediv__(self, other):
+        new_num = self.num * other.den
+        new_den = self.den * other.num
+        return "{}/{}".format(new_num, new_den)
+
+    def convert_to_decimal(self):
+        return self.num / self.den
+     
+
+fr1 = Fraction(3, 4)
+print(fr1) # 3/4
+fr2 = Fraction(1, 2)
+print(fr2) # # 1/2
+
+print(fr1 + fr2) # 10/8
+print(fr1 - fr2) # 2/8
+print(fr1 * fr2) # 3/8
+print(fr1 / fr2) # 6/4
+
+print(fr1.convert_to_decimal()) # 0.75
