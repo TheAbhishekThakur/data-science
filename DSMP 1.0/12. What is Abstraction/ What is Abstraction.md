3f7a7e468ee69f15b77738e441d95ab8bc3e9652
Abhishek <abhishek84411@gmail.com>
2025-08-16 18:32:24 +0530

    Add documentation and example script for abstraction concept in OOP

---
+++ DSMP 1.0/12. What is Abstraction/ What is Abstraction.md
@@ -0,0 +1,3 @@
+# What is Abstraction
+
+A technique used to manage complexity in software by hiding unnecessary implementation details and revealing only the essential functionalities to the user.
---
+++ DSMP 1.0/12. What is Abstraction/script.py
@@ -0,0 +1,23 @@
+# Abstraction
+
+from abc import ABC, abstractmethod
+
+class BankApp(ABC):
+    def database(self):
+        print("Connected to database")
+
+    @abstractmethod
+    def security(self):
+        pass
+
+class MobileApp(BankApp):
+    def mobile_login(self):
+        print("Login into mobile")
+
+    def security(self):
+        print("Mobile security")
+
+mob = MobileApp()
+mob.database()
+mob.mobile_login()
+mob.security()
